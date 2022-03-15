d8905794ac21de1c4fdc3b05ef458e7456db6b98
Zhu Haoyu <42167870+GoodKlaus@users.noreply.github.com>
2022-03-15 16:35:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-const initialRecord = [];
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
 function validate(name, no) {
   var reg_name = /^[a-z ,.'-]+$/i
   var reg_phone = /^\d{8}$/
@@ -17,12 +21,16 @@ function validate(name, no) {
 
 function TravellerRow(props) {
   const record = props.record;
+  var today = record.timestamp;
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date+' '+time;
   return (
     <tr>
       <td>{record.id}</td>
       <td>{record.name}</td>
       <td>{record.phone}</td>
-      <td>{record.timestamp}</td>
+      <td>{dateTime}</td>
     </tr>
   );
 }
@@ -114,6 +122,35 @@ class AddTraveller extends React.Component {
   }
 }
 
+class BlackList extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit_bl = this.handleSubmit_bl.bind(this);
+  }
+
+  handleSubmit_bl(e) {
+    e.preventDefault();
+    const form = document.forms.blackListAdd;
+    const name_bl = form.name.value;
+    const phone_bl = form.phone.value;
+    if (validate(name_bl, phone_bl)) {
+      this.props.createBlackList(name_bl, phone_bl);
+    }
+    form.name.value = "";
+    form.phone.value = "";
+  }
+
+  render() {
+    return (
+      <form name="blackListAdd" onSubmit={this.handleSubmit_bl}>
+        <input type="text" name="name" placeholder="Name for Blacklisting" />
+        <input type="text" name="phone" placeholder="Phone for Blacklisting" />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
 function DisplayFreeSeats(props) {
   const number = props.records.length;
   return (
@@ -155,16 +192,41 @@ class DrawSeat extends React.Component {
   }
 }
 
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({ query, variables })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+
+    if (result.errors) {
+      const error = result.errors[0];
+      alert(error.message);
+      if (error.extensions.code == 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+      }
+    }
+    return result.data;
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
 class DisplayHomepage extends React.Component {
   constructor() {
     super();
-    this.state = {records:[], add: false, delete: false, display: false, clickme: false};
+    this.state = {records:[], add: false, delete: false, display: false, black: false};
     this.createRecord = this.createRecord.bind(this);
     this.deleteRecord = this.deleteRecord.bind(this);
+    this.createBlackList = this.createBlackList.bind(this);
     this.hide = this.hide.bind(this);
     this.showAdd = this.showAdd.bind(this);
     this.showDel = this.showDel.bind(this);
     this.showDis = this.showDis.bind(this);
+    this.showBlack = this.showBlack.bind(this);
 
   }
 
@@ -172,76 +234,70 @@ class DisplayHomepage extends React.Component {
     this.loadData();
   }
 
-  loadData() {
-    setTimeout(() => {
-      this.setState({records: initialRecord});
-    }, 500);
+  async loadData() {
+    const query = `query {
+      recordList {
+        id name phone timestamp
+      }
+    }`;
+
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ records: data.recordList });
+    }
   }
 
-  createRecord(record) {
-    record.id = this.state.records.length + 1;
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
-    record.timestamp = dateTime;
-    const newRecordList = this.state.records.slice();
-
-    let find = false;
-    for (let i=0; i<newRecordList.length; i++) {
-      if(newRecordList[i].name == record.name && newRecordList[i].phone == record.phone) {
-        alert("Sorry, there is a booking under this name!");
-        find = true;
-        break;
+  async createRecord(record) {
+    const query = `mutation recordAdd($record: RecordInputs!) {
+      recordAdd(record: $record) {
+        id
       }
-    }
-    if(!find) {
-      newRecordList.push(record);
-      this.setState({records: newRecordList});
-      alert("You have successfully made your reservation!");
+    }`;
+
+    const data = await graphQLFetch(query, { record });
+    if (data) {
+      this.loadData();
     }
     
   }
 
-  deleteRecord(record_to_delete) {
-    let newRecordList = this.state.records.slice();
-    let target_ind = 0;
-    let find = false;
-    for (let i=0; i<newRecordList.length; i++) {
-      if(newRecordList[i].name == record_to_delete.name && newRecordList[i].phone == record_to_delete.phone) {
-        target_ind = i;
-        find = true;
-        break;
-      }
+  async deleteRecord(record_to_delete) {
+    const query = `mutation recordDelete($record_to_delete: RecordInputs!) {
+      recordDelete(record: $record_to_delete) 
+    }`;
+
+    const data = await graphQLFetch(query, { record_to_delete });
+    if (data) {
+      this.loadData();
     }
-    if(find) {
-      newRecordList.splice(target_ind, 1);
-      for(let i=target_ind; i<newRecordList.length; i++) {
-        newRecordList[i].id = i+1;
-      }
-      this.setState({records: newRecordList});
-      alert("You have successfully canceled your booking!");
-    }
-    else {
-      alert("Sorry, you don't have a booking yet!");
-    }
-    
+  }
+
+  async createBlackList(name, phone) {
+    const query = `mutation blAdd($name: String!, $phone: String!) {
+      createBlackList(name: $name, phone: $phone)
+    }`;
+
+    const data = await graphQLFetch(query, { name, phone });
   }
 
   hide() {
-    this.setState({add: false, delete: false, display: false});
+    this.setState({add: false, delete: false, display: false, black: false});
   }
 
   showAdd() {
-    this.setState({add: true, delete: false, display: false});
+    this.setState({add: true, delete: false, display: false, black: false});
   }
 
   showDel() {
-    this.setState({add: false, delete: true, display: false});
+    this.setState({add: false, delete: true, display: false, black: false});
   }
 
   showDis() {
-    this.setState({add: false, delete: false, display: true});
+    this.setState({add: false, delete: false, display: true, black: false});
+  }
+
+  showBlack() {
+    this.setState({add: false, delete: false, display: false, black: true});
   }
 
   render() {
@@ -255,12 +311,14 @@ class DisplayHomepage extends React.Component {
           <li><a onClick={this.showAdd}>Add Traveller</a></li>
           <li><a onClick={this.showDel}>Delete Traveller</a></li>
           <li><a onClick={this.showDis}>Display Traveller</a></li>
+          <li><a onClick={this.showBlack}>Add to Blacklist</a></li>
         </ul>
         </div>
         <hr />
         {this.state.add && (<AddTraveller createRecord={this.createRecord} records={this.state.records}/>)}
         {this.state.delete && (<DeleteTraveller deleteRecord={this.deleteRecord}/>)}
         {this.state.display && (<DisplayTraveller records={this.state.records}/>)}
+        {this.state.black && (<BlackList createBlackList={this.createBlackList}/>)}
         <hr />
         <DrawSeat records={this.state.records}/>
         <DisplayFreeSeats records={this.state.records} />
